463808f3ee3e6f3c6f4eb2ecdedfc16289d57d3f
მაცი <93665709+maci03@users.noreply.github.com>
2024-01-18 16:22:01 +0400

    work on ligththeme and darktheme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import {ThemeProvider} from "stled-components";
+import { lightTheme, darkTheme } from './Themes';
 
+const StyledApp = styled.div``;
 
 function App() {
   const [theme, setTheme] = useState("light");
   const themeToggler = () => {
         theme === "light" ? setTheme("dark") : setTheme("light");
   }
-  return <ThemeProvider>
-      <div className='App'></div>
+  return (
+  <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <StyledApp>hello</StyledApp>
   </ThemeProvider>
+  )
 }
 
 export default App;
--- src/themes.js
+++ src/themes.js
@@ -1,3 +1,9 @@
-const lightTheme = {
-    
-}
+export const lightTheme = {
+    body: "#fff",
+    fontColor: "#000"
+};
+
+export const darkTheme = {
+    body: "#000",
+    fontColor: "#fff"
+};
